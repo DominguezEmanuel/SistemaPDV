ae988adb0defd41240b04ab55c75fa504d33573d
DominguezEmanuel <dmanu401@gmail.com>
2026-07-27 12:29:50 -0300

    feat: agregar estructura del proyecto

---
+++ pdv-servidor/backend/src/test/java/com/sistemapdv/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sistemapdv.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
