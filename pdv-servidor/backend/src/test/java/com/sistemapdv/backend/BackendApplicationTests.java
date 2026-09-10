1d91d8571c84133375d7c3cde2e566930f07a290
DominguezEmanuel <dmanu401@gmail.com>
2026-09-10 19:07:28 -0300

    feat: agregar perfiles para pruebas

--- pdv-servidor/backend/src/test/java/com/sistemapdv/backend/BackendApplicationTests.java
+++ pdv-servidor/backend/src/test/java/com/sistemapdv/backend/BackendApplicationTests.java
@@ -2,8 +2,26 @@ package com.sistemapdv.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@ActiveProfiles("dev")
+@TestPropertySource(properties = {
+        "DB_URL_DEV=jdbc:postgresql://localhost:5432/pdv_dev",
+        "DB_USERNAME_DEV=postgres",
+        "DB_PASSWORD_DEV=admin",
+        "JWT_SECRET=xbUMgC1cuauaxExDABSsF4KEsfCXy9MXeV+bhuXC4Xs=",
+        "JWT_EXPIRATION=28800000",
+        "CLOUDINARY_CLOUD_NAME=w09zsldd",
+        "CLOUDINARY_API_KEY=296374333262985",
+        "CLOUDINARY_API_SECRET=9V5fzpklGXSwF6m88lXy8IikJYg",
+        "EMAIL_HOST=smtp.gmail.com",
+        "EMAIL_PORT=587",
+        "EMAIL_USER=dmanu401@gmail.com",
+        "EMAIL_PASSWORD=qliu itjl iyta uvic",
+        "EMAIL_ADMIN=dmanu401@gmail.com"
+})
 class BackendApplicationTests {
 
     @Test
