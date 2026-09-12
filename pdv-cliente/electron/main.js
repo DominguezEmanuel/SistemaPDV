c0da975f32520eb40e2c4babf7108388a99c72dc
DominguezEmanuel <dmanu401@gmail.com>
2026-09-12 17:39:49 -0300

    feat: agregar logo de la aplicación e ignora artefactos de compilación

--- pdv-cliente/electron/main.js
+++ pdv-cliente/electron/main.js
@@ -3,29 +3,42 @@ const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
 function createWindow() {
-  const win = new BrowserWindow({
-    width: 1400,
-    height: 900,
+  const mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
 
     webPreferences: {
       nodeIntegration: false,
       contextIsolation: true,
     },
 
-    icon: path.join(__dirname, "../public/logo.ico"),
-
-    //titleBarStyle: "hidden",
-
-    /*titleBarOverlay: {
-      color: "#2f3241",
-      symbolColor: "#74b1be",
-      height: 60,
-    },*/
+    icon: path.join(__dirname, "/assets/logo.ico"),
   });
 
-  win.loadURL("http://localhost:4200");
+  if (!app.isPackaged) {
+    // Desarrollo
+    mainWindow.loadURL("http://localhost:4200");
+  } else {
+    // Produccion
+    mainWindow.loadFile(
+      path.join(
+        __dirname,
+        "..",
+        "dist",
+        "pdv-cliente",
+        "browser",
+        "index.html",
+      ),
+    );
+  }
 }
 
 app.whenReady().then(() => {
   createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
