7e9cc64c2f6a1b6e54465018117c55edb002cd97
EmanuelDominguez <dmanu401@gmail.com>
2026-09-15 10:48:30 -0300

    feat: agregar migracion para datos iniciales

---
+++ pdv-servidor/backend/src/main/resources/db/migration/V3__insert_data_initial.sql
@@ -0,0 +1,17 @@
+-- Categorías
+INSERT INTO categorias (nombre)
+VALUES
+    ('Uñas'),
+    ('Pestañas'),
+    ('Maquillaje'),
+    ('Skincare'),
+    ('Depilacion'),
+    ('Peinados'),
+    ('Perfumeria'),
+    ('Pedicura');
+
+-- Canales de venta
+INSERT INTO canales_venta (nombre)
+VALUES
+    ('Local Fisico'),
+    ('Lives Tik Tok');
